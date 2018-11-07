61a7f34b071596a33e8b1cf618f4727fc9caa7c0
Hannah Hill <38006750+hilljhannah@users.noreply.github.com>
2018-11-07 14:50:23 -0600

    finished album-info.js file

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,6 +1,13 @@
 {
   // inserts album title
   $('#album-title').text(album.title);
+
   // album image
   $('img#album-cover-art').attr('src', album.albumArtUrl);
+
+  // artist info
+  $("h2.artist").text(album.artist);
+
+  // release info
+  $('#release-info').text(album.releaseInfo);
 }
