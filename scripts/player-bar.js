1093668710034e1da142e42b0115e436a131efe2
Hannah Hill <38006750+hilljhannah@users.noreply.github.com>
2018-11-19 13:19:49 -0600

    Assignment-9-Frontend-Intensive: complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -2,7 +2,7 @@
   $('button#play-pause').on('click', function() {
     player.playPause();
     $(this).attr('playState', player.playState);
-  });
+    });
 
     $('button#next').on('click', function() {
       if (player.playstate !== 'playing')  { return; }
@@ -14,4 +14,14 @@
       const nextSong = album.songs[nextSongIndex];
       player.playPause(nextSong);
     });
+
+    $('button#previous').on('click', function() {
+      if (player.playstate !== 'playing') { return; }
+
+      const previousSongIndex = currentSongIndex - 1;
+      if (previousSongIndex >= album.songs.length) { return; }
+
+      const previousSong = album.songs[previousSongIndex];
+      player.playPause(previousSong)
+    })
 }
