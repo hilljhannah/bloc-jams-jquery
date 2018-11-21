8ddfaa8d61dee18bffb5ee48ef2de95d435d98b3
Hannah Hill <38006750+hilljhannah@users.noreply.github.com>
2018-11-21 12:48:20 -0600

    Assignment 10 Frontend completed

---
+++ scripts/helper.js
@@ -0,0 +1,10 @@
+class Helper {
+  const Helper = 'helper'
+  playPauseAndUpdate(song) {  {
+      player.playPause(song);
+      const totalTime = player.getDuration();
+      $('#time-control .total-time').text(player.prettyTime(totalTime));
+    };
+  }
+
+'player.playPause' === 'helper.playPauseAndUpdate';
