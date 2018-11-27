adbcd50d023da291e0d3e8695248a778d1edbfc7
Hannah Hill <38006750+hilljhannah@users.noreply.github.com>
2018-11-27 15:47:40 -0600

    changes in capitalization

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,46 +1,46 @@
 {
     $('button#play-pause').on('click', function() {
-        Helper.playPauseAndUpdate();
-        $(this).attr('playState', Player.playState);
+        helper.playPauseAndUpdate();
+        $(this).attr('playState', player.playState);
     });
 
 
     $('button#next').on('click', function() {
-        if (Player.playstate !== 'playing')  { return; }
+        if (player.playstate !== 'playing')  { return; }
 
         const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
         const nextSongIndex = currentSongIndex + 1;
         if (nextSongIndex >= album.songs.length) { return; }
 
         const nextSong = album.songs[nextSongIndex];
-        Helper.playPauseAndUpdate(nextSong);
+        helper.playPauseAndUpdate(nextSong);
     });
 
 
     $('button#previous').on('click', function() {
-        if (Player.playstate !== 'playing') { return; }
+        if (player.playstate !== 'playing') { return; }
 
         const previousSongIndex = currentSongIndex - 1;
         if (previousSongIndex >= album.songs.length) { return; }
 
         const previousSong = album.songs[previousSongIndex];
-        Helper.playPauseAndUpdate(previousSong);
+        helper.playPauseAndUpdate(previousSong);
     });
 
 
     $('#time-control input').on('input', function (event) {
-        Player.skipTo(event.target.value);
+        player.skipTo(event.target.value);
       });
 
     $('#volume-control input').on('input', function (event){
-        Player.setVolume(event.target.value);
+        player.setVolume(event.target.value);
     })
 
 
     setInterval( () => {
-        if ( Player.playState !== 'playing' ) { return; }
-        const currentTime = Player.getTime();
-        const duration = Player.getDuration();
+        if ( player.playState !== 'playing' ) { return; }
+        const currentTime = player.getTime();
+        const duration = player.getDuration();
         const percent = (currentTime / duration) * 100;
         $('#time-control .current-time').text( currentTime );
         $('#time-control input').val(percent);
