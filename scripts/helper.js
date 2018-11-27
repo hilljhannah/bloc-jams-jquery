cfdd8fc8e9645dca8e6ea9ad1913deae0b1a6fb3
Hannah Hill <38006750+hilljhannah@users.noreply.github.com>
2018-11-27 09:34:22 -0600

    Assignment 10 Frontend Intensive: completed

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,10 +1,9 @@
 class Helper {
-  const Helper = 'helper'
   playPauseAndUpdate(song) {  {
       player.playPause(song);
       const totalTime = player.getDuration();
       $('#time-control .total-time').text(player.prettyTime(totalTime));
     };
   };
-
-'player.playPause' === 'helper.playPauseAndUpdate';
+  
+const Helper = new helper();
